6a129da7834d8e5f117975f54c0501a91706e3ed
Jerome Maye <jerome.maye@mavt.ethz.ch>
2013-05-08 10:38:15 +0000

    * adding diagnostics * restructuring

---
+++ include/janeth/PosLvNode.h
@@ -0,0 +1,151 @@
+/******************************************************************************
+ * Copyright (C) 2013 by Jerome Maye                                          *
+ * jerome.maye@gmail.com                                                      *
+ *                                                                            *
+ * This program is free software; you can redistribute it and/or modify       *
+ * it under the terms of the Lesser GNU General Public License as published by*
+ * the Free Software Foundation; either version 3 of the License, or          *
+ * (at your option) any later version.                                        *
+ *                                                                            *
+ * This program is distributed in the hope that it will be useful,            *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of             *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the              *
+ * Lesser GNU General Public License for more details.                        *
+ *                                                                            *
+ * You should have received a copy of the Lesser GNU General Public License   *
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.       *
+ ******************************************************************************/
+
+/** \file PosLvNode.h
+    \brief This file defines the PosLvNode class which implements the Applanix
+           POS LV node.
+  */
+
+#ifndef POSLV_NODE_H
+#define POSLV_NODE_H
+
+#include <string>
+#include <memory>
+
+#include <ros/ros.h>
+#include <diagnostic_updater/diagnostic_updater.h>
+
+class VehicleNavigationSolution;
+class VehicleNavigationPerformance;
+class TimeTaggedDMIData;
+class TCPConnectionClient;
+
+namespace diagnostic_updater {
+  class HeaderlessTopicDiagnostic;
+}
+
+namespace janeth {
+
+  /** The class PosLvNode implements the Applanix POSL LV node.
+      \brief POS LV node
+    */
+  class PosLvNode {
+  public:
+    /** \name Constructors/destructor
+      @{
+      */
+    /// Constructor
+    PosLvNode(const ros::NodeHandle& nh);
+    /// Copy constructor
+    PosLvNode(const PosLvNode& other) = delete;
+    /// Copy assignment operator
+    PosLvNode& operator = (const PosLvNode& other) = delete;
+    /// Move constructor
+    PosLvNode(PosLvNode&& other) = delete;
+    /// Move assignment operator
+    PosLvNode& operator = (PosLvNode&& other) = delete;
+    /// Destructor
+    virtual ~PosLvNode();
+    /** @}
+      */
+
+    /** \name Methods
+      @{
+      */
+    /// Spin once
+    void spin();
+    /** @}
+      */
+
+  protected:
+    /** \name Protected methods
+      @{
+      */
+    /// Publishes the vehicle navigation solution message
+    void publishVehicleNavigationSolution(const ros::Time& timestamp,
+      const VehicleNavigationSolution& vns);
+    /// Publishes the vehicle navigation performance message
+    void publishVehicleNavigationPerformance(const ros::Time& timestamp,
+      const VehicleNavigationPerformance& vnp);
+    /// Publishes the time-tagged DMI message
+    void publishTimeTaggedDMIData(const ros::Time& timestamp,
+      const TimeTaggedDMIData& dmi);
+    /// Diagnose the TCP connection
+    void diagnoseTCPConnection(diagnostic_updater::DiagnosticStatusWrapper&
+      status);
+    /// Diagnose the navigation status
+    void diagnoseNavStatus(diagnostic_updater::DiagnosticStatusWrapper& status);
+    /// Diagnose the alignment status
+    void diagnoseAlignStatus(diagnostic_updater::DiagnosticStatusWrapper&
+      status);
+    /** @}
+      */
+
+    /** \name Protected members
+      @{
+      */
+    /// ROS node handle
+    ros::NodeHandle _nodeHandle;
+    /// Vehicle navigation solution publisher
+    ros::Publisher _vehicleNavigationSolutionPublisher;
+    /// Vehicle navigation performance publisher
+    ros::Publisher _vehicleNavigationPerformancePublisher;
+    /// Time-tagged DMI data publisher
+    ros::Publisher _timeTaggedDMIDataPublisher;
+    /// Frame ID
+    std::string _frameId;
+    /// IP string
+    std::string _deviceIpStr;
+    /// Port
+    int _devicePort;
+    /// TCP connection
+    std::shared_ptr<TCPConnectionClient> _tcpConnection;
+    /// Retry timeout for TCP
+    double _retryTimeout;
+    /// Diagnostic updater
+    diagnostic_updater::Updater _updater;
+    /// Frequency diagnostic for vehicle navigation solution
+    std::shared_ptr<diagnostic_updater::HeaderlessTopicDiagnostic> _vnsFreq;
+    /// Vehicle navigation solution minimum frequency
+    double _vnsMinFreq;
+    /// Vehicle navigation solution maximum frequency
+    double _vnsMaxFreq;
+    /// Frequency diagnostic for vehicle navigation performance
+    std::shared_ptr<diagnostic_updater::HeaderlessTopicDiagnostic> _vnpFreq;
+    /// Vehicle navigation performance minimum frequency
+    double _vnpMinFreq;
+    /// Vehicle navigation performance maximum frequency
+    double _vnpMaxFreq;
+    /// Frequency diagnostic for time-tagged DMI data
+    std::shared_ptr<diagnostic_updater::HeaderlessTopicDiagnostic> _dmiFreq;
+    /// Time-tagged DMI data minimum frequency
+    double _dmiMinFreq;
+    /// Time-tagged DMI data maximum frequency
+    double _dmiMaxFreq;
+    /// Alignement status
+    uint8_t _alignStatus;
+    /// Navigation solution status
+    int8_t _navStatus;
+    /** @}
+      */
+
+  };
+
+}
+
+#endif // POSLV_NODE_H
---
+++ src/PosLvNode.cpp
@@ -0,0 +1,327 @@
+/******************************************************************************
+ * Copyright (C) 2013 by Jerome Maye                                          *
+ * jerome.maye@gmail.com                                                      *
+ *                                                                            *
+ * This program is free software; you can redistribute it and/or modify       *
+ * it under the terms of the Lesser GNU General Public License as published by*
+ * the Free Software Foundation; either version 3 of the License, or          *
+ * (at your option) any later version.                                        *
+ *                                                                            *
+ * This program is distributed in the hope that it will be useful,            *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of             *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the              *
+ * Lesser GNU General Public License for more details.                        *
+ *                                                                            *
+ * You should have received a copy of the Lesser GNU General Public License   *
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.       *
+ ******************************************************************************/
+
+#include "janeth/PosLvNode.h"
+
+#include <diagnostic_updater/publisher.h>
+
+#include <boost/shared_ptr.hpp>
+
+#include <libposlv/types/VehicleNavigationSolution.h>
+#include <libposlv/types/VehicleNavigationPerformance.h>
+#include <libposlv/types/TimeTaggedDMIData.h>
+#include <libposlv/types/PrimaryGPSStatus.h>
+#include <libposlv/com/TCPConnectionClient.h>
+#include <libposlv/sensor/POSLVComTCP.h>
+#include <libposlv/exceptions/IOException.h>
+#include <libposlv/exceptions/SystemException.h>
+#include <libposlv/base/Timer.h>
+#include <libposlv/types/Packet.h>
+#include <libposlv/types/Group.h>
+
+#include "poslv_ros/VehicleNavigationSolutionMsg.h"
+#include "poslv_ros/VehicleNavigationPerformanceMsg.h"
+#include "poslv_ros/TimeTaggedDMIDataMsg.h"
+
+namespace janeth {
+
+/******************************************************************************/
+/* Constructors and Destructor                                                */
+/******************************************************************************/
+
+  PosLvNode::PosLvNode(const ros::NodeHandle& nh) :
+      _nodeHandle(nh),
+      _alignStatus(8),
+      _navStatus(-1) {
+    _nodeHandle.param<std::string>("frame_id", _frameId, "vehicle_base_link");
+    _nodeHandle.param<std::string>("device_ip", _deviceIpStr, "129.132.39.171");
+    _nodeHandle.param<int>("device_port", _devicePort, 5602);
+    _nodeHandle.param<double>("retry_timeout", _retryTimeout, 1);
+    _nodeHandle.param<double>("vns_min_freq", _vnsMinFreq, 1);
+    _nodeHandle.param<double>("vns_max_freq", _vnsMaxFreq, 200);
+    _nodeHandle.param<double>("vnp_min_freq", _vnpMinFreq, 0.5);
+    _nodeHandle.param<double>("vnp_max_freq", _vnpMaxFreq, 1);
+    _nodeHandle.param<double>("dmi_min_freq", _dmiMinFreq, 1);
+    _nodeHandle.param<double>("dmi_max_freq", _dmiMaxFreq, 200);
+    const int queueDepth = 100;
+    _vehicleNavigationSolutionPublisher =
+      _nodeHandle.advertise<poslv_ros::VehicleNavigationSolutionMsg>(
+      "vehicle_navigation_solution", queueDepth);
+    _vehicleNavigationPerformancePublisher =
+      _nodeHandle.advertise<poslv_ros::VehicleNavigationPerformanceMsg>(
+      "vehicle_navigation_performance", queueDepth);
+    _timeTaggedDMIDataPublisher =
+      _nodeHandle.advertise<poslv_ros::TimeTaggedDMIDataMsg>(
+      "time_tagged_dmi_data", queueDepth);
+    _updater.setHardwareID("none");
+    _updater.add("TCP connection", this, &PosLvNode::diagnoseTCPConnection);
+    _updater.add("Alignement status", this, &PosLvNode::diagnoseAlignStatus);
+    _updater.add("Navigation status", this, &PosLvNode::diagnoseNavStatus);
+    _vnsFreq.reset(new diagnostic_updater::HeaderlessTopicDiagnostic(
+      "vehicle_navigation_solution", _updater,
+      diagnostic_updater::FrequencyStatusParam(&_vnsMinFreq, &_vnsMaxFreq,
+      0.1, 10)));
+    _vnpFreq.reset(new diagnostic_updater::HeaderlessTopicDiagnostic(
+      "vehicle_navigation_performance", _updater,
+      diagnostic_updater::FrequencyStatusParam(&_vnpMinFreq, &_vnpMaxFreq,
+      0.1, 10)));
+    _dmiFreq.reset(new diagnostic_updater::HeaderlessTopicDiagnostic(
+      "time_tagged_dmi_data", _updater,
+      diagnostic_updater::FrequencyStatusParam(&_dmiMinFreq, &_dmiMaxFreq,
+      0.1, 10)));
+    _updater.force_update();
+  }
+
+  PosLvNode::~PosLvNode() {
+  }
+
+/******************************************************************************/
+/* Methods                                                                    */
+/******************************************************************************/
+
+  void PosLvNode::publishVehicleNavigationSolution(const ros::Time& timestamp,
+      const VehicleNavigationSolution& vns) {
+    boost::shared_ptr<poslv_ros::VehicleNavigationSolutionMsg> vnsMsg(
+      new poslv_ros::VehicleNavigationSolutionMsg);
+    vnsMsg->header.stamp = timestamp;
+    vnsMsg->header.frame_id = _frameId;
+    vnsMsg->TimeDistance.Time1 = vns.mTimeDistance.mTime1;
+    vnsMsg->TimeDistance.Time2 = vns.mTimeDistance.mTime2;
+    vnsMsg->TimeDistance.DistanceTag = vns.mTimeDistance.mDistanceTag;
+    vnsMsg->TimeDistance.TimeType = vns.mTimeDistance.mTimeType;
+    vnsMsg->TimeDistance.DistanceType = vns.mTimeDistance.mDistanceType;
+    vnsMsg->Latitude = vns.mLatitude;
+    vnsMsg->Longitude = vns.mLongitude;
+    vnsMsg->Altitude = vns.mAltitude;
+    vnsMsg->NorthVelocity = vns.mNorthVelocity;
+    vnsMsg->EastVelocity = vns.mEastVelocity;
+    vnsMsg->DownVelocity = vns.mDownVelocity;
+    vnsMsg->Roll = vns.mRoll;
+    vnsMsg->Pitch = vns.mPitch;
+    vnsMsg->Heading = vns.mHeading;
+    vnsMsg->WanderAngle = vns.mWanderAngle;
+    vnsMsg->TrackAngle = vns.mTrackAngle;
+    vnsMsg->Speed = vns.mSpeed;
+    vnsMsg->AngularRateLong = vns.mAngularRateLong;
+    vnsMsg->AngularRateTrans = vns.mAngularRateTrans;
+    vnsMsg->AngularRateDown = vns.mAngularRateDown;
+    vnsMsg->AccLong = vns.mAccLong;
+    vnsMsg->AccTrans = vns.mAccTrans;
+    vnsMsg->AccDown = vns.mAccDown;
+    vnsMsg->AlignementStatus = vns.mAlignementStatus;
+    _vehicleNavigationSolutionPublisher.publish(vnsMsg);
+    _alignStatus = vns.mAlignementStatus;
+    _vnsFreq->tick();
+  }
+
+  void PosLvNode::publishVehicleNavigationPerformance(
+      const ros::Time& timestamp, const VehicleNavigationPerformance& vnp) {
+    boost::shared_ptr<poslv_ros::VehicleNavigationPerformanceMsg> vnpMsg(
+      new poslv_ros::VehicleNavigationPerformanceMsg);
+    vnpMsg->header.stamp = timestamp;
+    vnpMsg->header.frame_id = _frameId;
+    vnpMsg->TimeDistance.Time1 = vnp.mTimeDistance.mTime1;
+    vnpMsg->TimeDistance.Time2 = vnp.mTimeDistance.mTime2;
+    vnpMsg->TimeDistance.DistanceTag = vnp.mTimeDistance.mDistanceTag;
+    vnpMsg->TimeDistance.TimeType = vnp.mTimeDistance.mTimeType;
+    vnpMsg->TimeDistance.DistanceType = vnp.mTimeDistance.mDistanceType;
+    vnpMsg->NorthPositionRMSError = vnp.mNorthPositionRMSError;
+    vnpMsg->EastPositionRMSError = vnp.mEastPositionRMSError;
+    vnpMsg->DownPositionRMSError = vnp.mDownPositionRMSError;
+    vnpMsg->NorthVelocityRMSError = vnp.mNorthVelocityRMSError;
+    vnpMsg->EastVelocityRMSError = vnp.mEastVelocityRMSError;
+    vnpMsg->DownVelocityRMSError = vnp.mDownVelocityRMSError;
+    vnpMsg->RollRMSError = vnp.mRollRMSError;
+    vnpMsg->PitchRMSError = vnp.mPitchRMSError;
+    vnpMsg->HeadingRMSError = vnp.mHeadingRMSError;
+    vnpMsg->ErrorEllipsoidSemiMajor = vnp.mErrorEllipsoidSemiMajor;
+    vnpMsg->ErrorEllipsoidSemiMinor = vnp.mErrorEllipsoidSemiMinor;
+    vnpMsg->ErrorEllipsoidOrientation = vnp.mErrorEllipsoidOrientation;
+    _vehicleNavigationPerformancePublisher.publish(vnpMsg);
+    _vnpFreq->tick();
+  }
+
+  void PosLvNode::publishTimeTaggedDMIData(
+      const ros::Time& timestamp, const TimeTaggedDMIData& dmi) {
+    boost::shared_ptr<poslv_ros::TimeTaggedDMIDataMsg> dmiMsg(
+      new poslv_ros::TimeTaggedDMIDataMsg);
+    dmiMsg->header.stamp = timestamp;
+    dmiMsg->header.frame_id = _frameId;
+    dmiMsg->TimeDistance.Time1 = dmi.mTimeDistance.mTime1;
+    dmiMsg->TimeDistance.Time2 = dmi.mTimeDistance.mTime2;
+    dmiMsg->TimeDistance.DistanceTag = dmi.mTimeDistance.mDistanceTag;
+    dmiMsg->TimeDistance.TimeType = dmi.mTimeDistance.mTimeType;
+    dmiMsg->TimeDistance.DistanceType = dmi.mTimeDistance.mDistanceType;
+    dmiMsg->SignedDistanceTraveled = dmi.mSignedDistanceTraveled;
+    dmiMsg->UnsignedDistanceTraveled = dmi.mUnsignedDistanceTraveled;
+    dmiMsg->DMIScaleFactor = dmi.mDMIScaleFactor;
+    dmiMsg->DataStatus = dmi.mDataStatus;
+    dmiMsg->DMIType = dmi.mDMIType;
+    dmiMsg->DMIDataRate = dmi.mDMIDataRate;
+    _timeTaggedDMIDataPublisher.publish(dmiMsg);
+    _dmiFreq->tick();
+  }
+
+  void PosLvNode::diagnoseTCPConnection(
+      diagnostic_updater::DiagnosticStatusWrapper& status) {
+    if (_tcpConnection != nullptr && _tcpConnection->isOpen())
+      status.summaryf(diagnostic_msgs::DiagnosticStatus::OK,
+        "TCP connection opened on %s:%d.",
+        _tcpConnection->getServerIP().c_str(),
+        _tcpConnection->getPort());
+    else
+     status.summaryf(diagnostic_msgs::DiagnosticStatus::ERROR,
+      "TCP connection closed.");
+  }
+
+  void PosLvNode::diagnoseAlignStatus(
+      diagnostic_updater::DiagnosticStatusWrapper& status) {
+    switch (_alignStatus) {
+      case 0:
+        status.summaryf(diagnostic_msgs::DiagnosticStatus::OK,
+          "Full navigation");
+        break;
+      case 1:
+        status.summaryf(diagnostic_msgs::DiagnosticStatus::WARN,
+          "Fine alignment is active");
+        break;
+      case 2:
+        status.summaryf(diagnostic_msgs::DiagnosticStatus::WARN,
+          "GC CHI 2");
+        break;
+      case 3:
+        status.summaryf(diagnostic_msgs::DiagnosticStatus::WARN,
+          "PC CHI 2");
+        break;
+      case 4:
+        status.summaryf(diagnostic_msgs::DiagnosticStatus::WARN,
+          "GC CHI 1");
+        break;
+      case 5:
+        status.summaryf(diagnostic_msgs::DiagnosticStatus::WARN,
+          "PC CHI 1");
+        break;
+      case 6:
+        status.summaryf(diagnostic_msgs::DiagnosticStatus::WARN,
+          "Coarse leveling is active");
+        break;
+      case 7:
+        status.summaryf(diagnostic_msgs::DiagnosticStatus::WARN,
+          "Initial solution assigned");
+        break;
+      case 8:
+        status.summaryf(diagnostic_msgs::DiagnosticStatus::ERROR,
+          "No valid solution");
+        break;
+      default:
+        break;
+    }
+  }
+
+  void PosLvNode::diagnoseNavStatus(
+      diagnostic_updater::DiagnosticStatusWrapper& status) {
+    switch (_navStatus) {
+      case -1:
+        status.summaryf(diagnostic_msgs::DiagnosticStatus::ERROR,
+          "Unkown");
+        break;
+      case 0:
+        status.summaryf(diagnostic_msgs::DiagnosticStatus::ERROR,
+          "No data from receiver");
+        break;
+      case 1:
+        status.summaryf(diagnostic_msgs::DiagnosticStatus::WARN,
+          "Horizontal C/A mode");
+        break;
+      case 2:
+        status.summaryf(diagnostic_msgs::DiagnosticStatus::WARN,
+          "3-dimension C/A mode");
+        break;
+      case 3:
+        status.summaryf(diagnostic_msgs::DiagnosticStatus::WARN,
+          "Horizontal DGPS mode");
+        break;
+      case 4:
+        status.summaryf(diagnostic_msgs::DiagnosticStatus::WARN,
+          "3-dimension DGPS mode");
+        break;
+      case 5:
+        status.summaryf(diagnostic_msgs::DiagnosticStatus::WARN,
+          "Float RTK mode");
+        break;
+      case 6:
+        status.summaryf(diagnostic_msgs::DiagnosticStatus::WARN,
+          "Integer wide lane RTK mode");
+        break;
+      case 7:
+        status.summaryf(diagnostic_msgs::DiagnosticStatus::ERROR,
+          "Integer narrow lane RTK mode");
+        break;
+      case 8:
+        status.summaryf(diagnostic_msgs::DiagnosticStatus::ERROR,
+          "P-Code");
+        break;
+      default:
+        break;
+    }
+  }
+
+  void PosLvNode::spin() {
+    _tcpConnection.reset(new TCPConnectionClient(_deviceIpStr, _devicePort));
+    POSLVComTCP device(*_tcpConnection);
+    Timer timer;
+    while (_nodeHandle.ok()) {
+      try {
+        std::shared_ptr<Packet> packet = device.readPacket();
+        const ros::Time timestamp = ros::Time::now();
+        const Group& group = packet->groupCast();
+        if (group.instanceOf<VehicleNavigationSolution>()) {
+          const VehicleNavigationSolution& vns =
+            group.typeCast<VehicleNavigationSolution>();
+          publishVehicleNavigationSolution(timestamp, vns);
+        }
+        else if (group.instanceOf<VehicleNavigationPerformance>()) {
+          const VehicleNavigationPerformance& vnp =
+            group.typeCast<VehicleNavigationPerformance>();
+          publishVehicleNavigationPerformance(timestamp, vnp);
+        }
+        else if (group.instanceOf<TimeTaggedDMIData>()) {
+          const TimeTaggedDMIData& dmi = group.typeCast<TimeTaggedDMIData>();
+          publishTimeTaggedDMIData(timestamp, dmi);
+        }
+        else if (group.instanceOf<PrimaryGPSStatus>()) {
+          const PrimaryGPSStatus& gps = group.typeCast<PrimaryGPSStatus>();
+          _navStatus = gps.mNavigationSolutionStatus;
+        }
+      } 
+      catch (const IOException& e) {
+        ROS_WARN_STREAM("IOException: " << e.what());
+        ROS_WARN_STREAM("Retrying in " << _retryTimeout << " [s]");
+        timer.sleep(_retryTimeout);
+      }
+      catch (SystemException& e) {
+        ROS_WARN_STREAM("SystemException: " << e.what());
+        ROS_WARN_STREAM("Retrying in " << _retryTimeout << " [s]");
+        timer.sleep(_retryTimeout);
+      }
+      _updater.update();
+      ros::spinOnce();
+    }
+  }
+
+}
